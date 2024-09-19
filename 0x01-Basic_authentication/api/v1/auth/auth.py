817fe9a8f5604d5b03695e90f20597f1ad4db2f5
Kelechi Ebiri <ebirikele@gmail.com>
2024-09-20 00:09:46 +0100

    Request Validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -43,7 +43,13 @@ class Auth:
         Returns:
             str: None for now, will be implemented later.
         """
-        return None
+        if request is None:
+            return None
+
+        if not request.headers.get('Authorization'):
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar:
         """
