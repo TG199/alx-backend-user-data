5ede563f6999e0f31bc757e1921946fb1f706d66
Kelechi Ebiri <ebirikele@gmail.com>
2024-09-20 00:59:29 +0100

    Basic64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,5 +5,27 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """ BasicAuth class that inherits from Auth """
-    pass
+    """BasicAuth class that inherits from Auth"""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization
+        header for Basic Authentication.
+
+        :param authorization_header: The Authorization
+        header from the request.
+        :return: The Base64 encoded part of the Authorization
+        header or None.
+        """
+
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[len("Basic "):]
