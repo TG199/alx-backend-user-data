ad10e664e4dcd2c2b9fc3d0aebde3c1f1561ffb4
Kelechi Ebiri <ebirikele@gmail.com>
2024-09-19 03:40:16 +0100

    Unathorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_test():
+    """Route to trigger a 401 error for testing purposes."""
+    abort(401)
