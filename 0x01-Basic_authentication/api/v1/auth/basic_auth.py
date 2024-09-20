ea1b89033eff3a3144a8cc10bb469bd9f4ca7f80
Kelechi Ebiri <ebirikele@gmail.com>
2024-09-20 01:29:45 +0100

    extend extract user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -79,7 +79,7 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
 
-        email, password = decoded_base64_authorization_header.split(':', 1)
+        email, password = decoded_base64_authorization_header.rsplit(':', 1)
 
         return email, password
 
