d910dad3c8cd047a49b09066c6d4d44ac644b382
Kelechi Ebiri <ebirikele@gmail.com>
2024-09-20 01:06:31 +0100

    decode method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Auth Module
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -29,3 +30,28 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+                                            self,
+                                            base64_authorization_header:
+                                            str) -> str:
+        """
+        Decodes the Base64 part of
+        the Authorization header.
+
+        :param base64_authorization_header: Base64 encoded string
+        from the Authorization header.
+        :return: Decoded string as UTF-8 or None if invalid.
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
