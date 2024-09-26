d5e45f3e49f308b764a00781dfe5ac4cf37b1b9b
Kelechi Ebiri <ebirikele@gmail.com>
2024-09-27 00:25:18 +0100

    User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/ur/bin/env python3
+""" User Model"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ User class model"""
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String, nullable=False, unique=True)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
+
+    def __repr__(self):
+        """ class rep"""
+        return f"<User(id={self.id}, email={self.email})>"
