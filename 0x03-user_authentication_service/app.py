deef00d84f5034801f38d1b86fe2d09c0ecabce7
Kelechi Ebiri <ebirikele@gmail.com>
2024-09-27 01:39:17 +0100

    user route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 """ Simple Flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+
+
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
 def welcome():
     """Returns a JSON response with a welcome message"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """Registers a new user if email is not already registered."""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        return jsonify({"message": "email and password are required"}), 400
 
 
 if __name__ == "__main__":
